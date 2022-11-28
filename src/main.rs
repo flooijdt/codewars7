97d45dc0c50ed84e84db467ba52074acd2b4bbe3
flooijdt <flooijdt@localhost.localdomain>
2022-11-28 10:56:34 -0300

    made it

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    in_array();
+}
+fn in_array() {
+    let toda = String::from("tifoide");
+    let part = String::from("oide");
+
+    if toda.contains(part.as_str()) {
+        println!("contem carai");
+    }
 }
