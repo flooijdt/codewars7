6de80ea59fe12cd85bd83641762b13d8db9cf98b
flooijdt <flooijdt@localhost.localdomain>
2022-11-28 10:57:35 -0300

    made it

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 fn main() {
-    in_array();
+    in_array(["sd"], ["a"]);
 }
-fn in_array() {
+fn in_array(arr_a: &[&str], arr_b: &[&str]) -> Vec<String> {
     let toda = String::from("tifoide");
     let part = String::from("oide");
 
-    if toda.contains(part.as_str()) {
+    if toda.contains(part) {
         println!("contem carai");
     }
+    Vec::new([toda])
 }
